3eb12eeaa20dc9816b06acd603ee92339d5c00f1
Nikolay Orozov <nikolay.orozov@q-perior.com>
2024-03-20 17:45:05 +0100

    enabled service delegate implementation and integration test

--- src/main/java/de/nikoconsulting/demo/delegate/EntityApiDelegateImpl.java
+++ src/main/java/de/nikoconsulting/demo/delegate/EntityApiDelegateImpl.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-//@Service
+@Service
 public class EntityApiDelegateImpl implements EntityApiDelegate {
 
     private final List<GenericEntity> entityList = new ArrayList<>();
--- src/test/java/de/nikoconsulting/demo/IntegrationTest.java
+++ src/test/java/de/nikoconsulting/demo/IntegrationTest.java
@@ -1,36 +1,69 @@
 package de.nikoconsulting.demo;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.nikoconsulting.demo.model.GenericEntity;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class IntegrationTest {
 
-
     @Autowired
     private MockMvc mockMvc;
 
+    private static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Test
-    public void whenReadAll_thenStatusIsNotImplemented() throws Exception {
+    public void whenReadAll_thenStatusIsOk() throws Exception {
         this.mockMvc.perform(MockMvcRequestBuilders.get("/api/v3/entity/all"))
-                .andExpect(MockMvcResultMatchers.status().isNotImplemented());
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
     @Test
-    public void whenReadOne_thenStatusIsNotImplemented() throws Exception {
+    public void whenReadOne_thenStatusIsOk() throws Exception {
         this.mockMvc.perform(MockMvcRequestBuilders.get("/api/v3/entity/findby/" + 1))
-                .andExpect(MockMvcResultMatchers.status().isNotImplemented());
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.value").value("entity_1"));
     }
 
+    @Test
+    public void whenReadFive_thenStatusIsNotFound() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/api/v3/entity/findby/" + 5))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    public void whenCreateNew_thenStatusIsCreated() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/api/v3/entity")
+                        .content(asJsonString(new GenericEntity(5L, "test_entity_5")))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+
+    @Test
+    public void whenCreateSame_thenStatusIsCreated() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/api/v3/entity")
+                        .content(asJsonString(new GenericEntity(4L, "test_entity_4")))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isConflict());
+    }
 }
